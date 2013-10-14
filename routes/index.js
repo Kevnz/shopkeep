05b55be4eada72003e5ec6e370f6c4eb51635038
Kevin Isom <kevin.isom@gmail.com>
2013-10-14 22:01:33 +1300

    added try/catch

--- routes/index.js
+++ routes/index.js
@@ -7,48 +7,51 @@ exports.customers = function(req, res){
 var qconf = require('qconf'),
     config = qconf();
 exports.saveCustomer = function (req, res) {
-    var Guid = require('guid');
-    var db = require('../lib/db')('customer');
-    var customers =  require('../lib/db')('customer');
-    console.log(customers);
-    var customer = {};
-    var cards =  require('../lib/db')('cc_details');
-    customer.first_name = req.body.first_name;
-    customer.last_name = req.body.last_name;
-    customer.email = req.body.email;
-    customer.created_on = new Date();
-    customer.id = Guid.create().toString();
+    try {
+        var Guid = require('guid');
+        var db = require('../lib/db')('customer');
+        var customers =  require('../lib/db')('customer');
+        console.log(customers);
+        var customer = {};
+        customer.first_name = req.body.first_name;
+        customer.last_name = req.body.last_name;
+        customer.email = req.body.email;
+        customer.created_on = new Date();
+        customer.id = Guid.create().toString();
 
-    var donation = {};
-    donation.amount = req.body.amount;
-    donation.repeat = req.body.repeat ? true : false;
+        var donation = {};
+        donation.amount = req.body.amount;
+        donation.repeat = req.body.repeat ? true : false;
 
-    customers.save(customer, function (err, obj) {
-        if(err) {
-            res.send(500, {error: 'something is wrong'});
-        } else {
-            donation.reference = 'Payment from user ' + customer.id;
-            var pxpay = require('pxpay');
+        customers.save(customer, function (err, obj) {
+            if(err) {
+                res.send(500, {error: 'something is wrong'});
+            } else {
+                donation.reference = 'Payment from user ' + customer.id;
+                var pxpay = require('pxpay');
 
-            var transaction = {
-                user: config.get('dps-user'),
-                password: config.get('dps-password'),
-                amount:  donation.amount + '.00',
-                reference: 'Payment from user ' + customer.id,
-                email: customer.email,
-                TxnId: 'trans-'+ Guid.create().toString(),
-                billingId: 'bd-' + Date.now(),
-                addCard: donation.repeat ? 1 : 0,
-                successURL: 'http://localhost:4567/success?user='+ customer.id,
-                failURL: 'http://localhost:4567/fail?user='+ customer.id
-            };
-            console.log(transaction);
-            pxpay.request(transaction, function(err, result) {
-                var url = result.URI;
-                res.redirect(url);
-            });
-            
-        }
+                var transaction = {
+                    user: config.get('dps-user'),
+                    password: config.get('dps-password'),
+                    amount:  donation.amount + '.00',
+                    reference: 'Payment from user ' + customer.id,
+                    email: customer.email,
+                    TxnId: 'trans-'+ Guid.create().toString(),
+                    billingId: 'bd-' + Date.now(),
+                    addCard: donation.repeat ? 1 : 0,
+                    successURL: 'http://localhost:4567/success?user='+ customer.id,
+                    failURL: 'http://localhost:4567/fail?user='+ customer.id
+                };
+                console.log(transaction);
+                pxpay.request(transaction, function(err, result) {
+                    var url = result.URI;
+                    res.redirect(url);
+                });
+                
+            }
 
-    });
+        });
+    } catch(failed) {
+        res.send(200, failed);
+    }
 };
