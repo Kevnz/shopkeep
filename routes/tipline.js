432daef175309b5a300d13f5a1f64ff45f0b635a
Kevin Isom <kevin.isom@gmail.com>
2013-10-15 11:26:38 +1300

    fighting cors

--- routes/tipline.js
+++ routes/tipline.js
@@ -10,6 +10,15 @@ var logtastic = require('../lib/db')('logtastic');
 
 
 exports.saveTip = function (req, res) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    
     var saveFile = function saveFileFromPost (file, id, callback) {
         try {
 
