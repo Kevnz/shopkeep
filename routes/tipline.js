170d72d1ed43bb734922de1c04c575e9e459c773
Kevin Isom <kevin.isom@gmail.com>
2013-10-24 07:29:30 +1300

    debugging stuff

--- routes/tipline.js
+++ routes/tipline.js
@@ -82,7 +82,7 @@ exports.saveTip = function (req, res) {
                 if(err) {
                     res.send(500, {error: 'something is wrong'});
                 } else {
-                    res.send(200);
+                    res.send(200, {message: 'all good', tipster:tipster});
                 }
             });
         });
