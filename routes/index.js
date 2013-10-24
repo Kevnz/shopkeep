50ce508a8186fbd344cab982808cc47b3271e155
Kevin Isom <kevin.isom@gmail.com>
2013-10-24 14:07:55 +1300

    removed redirect

--- routes/index.js
+++ routes/index.js
@@ -62,9 +62,7 @@ exports.saveCustomer = function (req, res) {
                 };
                 logger.log('createCustomer');
                 shopify.createCustomer(customer, function (err, shopifyCustomer) {
-                    if(err) {
-                        res.redirect('http://taxpayers.org.nz/account/register?error=true');
-                    }
+
                     logger.log('created shopify');
                     logger.logObject(err);
                     logger.logObject(shopifyCustomer);
