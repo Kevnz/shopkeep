7251f4284de1276ed9c4e6a70499168a8faf387e
Kevin Isom <kevin.isom@gmail.com>
2013-10-26 11:21:12 +1300

    added xhr headers

--- routes/details.js
+++ routes/details.js
@@ -1,4 +1,12 @@
 exports.index = function (req, res) {
+            res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
     var email = req.query.email;
 
     var customers = require('../lib/db')('customer');
@@ -13,10 +21,18 @@ exports.index = function (req, res) {
 };
 
 exports.update = function (req, res) {
+            res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
     var email = req.body.email;
     var amount = req.body.amount;
     var repeat = req.body.repeat;
-    
+
     var customers = require('../lib/db')('customer');
 
     customers.findOne({email: email}, function (err, doc) {
