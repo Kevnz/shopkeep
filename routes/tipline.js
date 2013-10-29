119ea044b0033c2e1790a90851b6fa1ae4f45a12
Kevin Isom <kevin.isom@gmail.com>
2013-10-30 00:07:12 +1300

    tips and headers

--- routes/tipline.js
+++ routes/tipline.js
@@ -11,12 +11,9 @@ var logtastic = require('../lib/db')('logtastic');
 
 exports.saveTip = function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    
+ 
 
     try {
         var Guid = require('guid');
