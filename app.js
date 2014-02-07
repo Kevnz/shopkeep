5e2d389f33360df4b056ec8342b4dc2afd1fb8eb
Kevin Isom <kevin.isom@gmail.com>
2014-02-08 09:28:27 +1300

    added http back to app

--- app.js
+++ app.js
@@ -232,10 +232,8 @@ app.get('/admin/xero/import/invoices/', auth, admin.pumpCustomersInvoices);
 app.get('/rebillsuccess', function(req, res) {
     console.log('rebill')
 })
-exports.app = app;
-/*
+ 
 http.createServer(app).listen(app.get('port'), function(){
     console.log("Express server listening on port " + app.get('port') + " in " + app.get('env') +" mode");
 });
 
-*/
