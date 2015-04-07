084b7966cca4ced4cde9bbca317805f11bb77d60
Kevin Isom <kevin.isom@gmail.com>
2015-04-06 21:32:59 -0400

    POS react app

---
+++ apps/pos/app.js
@@ -0,0 +1,6 @@
+'use strict';
+import React from 'react';
+import Shopkeep from './components/shopkeep-app';
+var appElement = document.getElementById('app');
+
+React.render(<Shopkeep  />, appElement);
