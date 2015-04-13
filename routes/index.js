53c72ce5f4a334520b2efb376f6aaa112d2df754
Kevin Isom <kevin.isom@gmail.com>
2015-04-13 09:41:37 -0400

    oops

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Adelphos' });
+  res.render('index', { title: 'Shopkeep' });
 });
 
 export default router;
