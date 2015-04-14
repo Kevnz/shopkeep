fa620823a605e7ffa48755d5c5d8c76a52df7036
Kevin Isom <kevin.isom@gmail.com>
2015-04-14 16:09:40 -0400

    don't do something stupid at this stage.

--- migrations/20150414034016-slug-products.js
+++ migrations/20150414034016-slug-products.js
@@ -4,19 +4,7 @@ var type = dbm.dataType;
 var to = require('to-case');
 
 exports.up = function(db, callback) {
-    console.log(db);
-    var products = require('mongo-start')('products');
-    var bulk = products.initializeOrderedBulkOp();
-
-    products.find(function (err, docs) {
-        for (var i = 0; i < docs.length; i++) {
-            bulk.find({_id: docs[i]._id}).update({$set:{slug:to.slug(docs[0]) }});
-        }
-    });
-    bulk.execute(function(err, res) {
-        callback();
-    });
-  
+    db.addIndex('products', 'product_slug', 'slug', false, callback);
 };
 
 exports.down = function(db, callback) {
