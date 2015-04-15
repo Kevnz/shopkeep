a6b08cf07158626b455bc823e8adb6ec4eb2d2e7
Kevin Isom <kevin.isom@gmail.com>
2015-04-15 07:52:44 -0400

    get some data

--- apps/shopping/app.js
+++ apps/shopping/app.js
@@ -2,5 +2,8 @@
 import React from 'react';
 import Shopkeep from './components/shopkeep-app';
 var appElement = document.getElementById('app');
+var api = require('./utils/web-api');
+
+api.getAllProducts();
 
 React.render(<Shopkeep  />, appElement);
