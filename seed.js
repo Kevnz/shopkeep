12d5bafb0f93d256a1bc3abf288a7992306a5dd8
Kevin Isom <kevin.isom@gmail.com>
2015-04-14 21:11:56 -0400

    cause price

--- seed.js
+++ seed.js
@@ -4,11 +4,13 @@ require('babel/register');
 var casual = require('casual');
 var products = require('mongo-start')('products');
 var addProduct = require('./seeds/products').addProduct;
+var to = require('to-case');
 addProduct(casual);
 var bulk = products.initializeOrderedBulkOp();
 
 for (var i = 0; i < 100; i++) {
     var product = casual.product;
+    product.slug = to.slug(product.name);
     bulk.insert(product)
 }
 bulk.execute(function(err, res) {
