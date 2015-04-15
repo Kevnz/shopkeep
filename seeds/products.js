b7ecfd0aaf7c224514ec234375ec54d95b2f76c2
Kevin Isom <kevin.isom@gmail.com>
2015-04-15 07:55:25 -0400

    Changing the shape of the data

--- seeds/products.js
+++ seeds/products.js
@@ -1,12 +1,30 @@
 'use strict';
 export function addProduct (casual) {
+    var catagories = ['Interesting', 'Exciting', 'Outrageous'];
     casual.define('product', function() {
+
         return {
             name: casual.catch_phrase,
             description: casual.description,
-            catagory: 'Test',
-            images: [{name:casual.words(4), url:'http://placehold.it/350x150'},
-            {name:casual.words(5), url:'http://placehold.it/350x150'}]
+            catagory: catagories[casual.integer(0,2)],
+            price: casual.integer(1,213) + '.' + casual.integer(10,99),
+            images: [{
+                name:casual.words(4), 
+                url: 'http://placehold.it/350x150'+ casual.rgb_hex.replace('#','/') + casual.rgb_hex.replace('#','/') 
+            },
+            {
+                name:casual.words(5),
+                url: 'http://placehold.it/350x150'+ casual.rgb_hex.replace('#','/') + casual.rgb_hex.replace('#','/') 
+            },
+            {
+                name:casual.words(5),
+                url: 'http://placehold.it/350x150'+ casual.rgb_hex.replace('#','/') + casual.rgb_hex.replace('#','/') 
+            },
+            {
+                name:casual.words(5),
+                url: 'http://placehold.it/350x150'+ casual.rgb_hex.replace('#','/') + casual.rgb_hex.replace('#','/') 
+            }
+            ]
         };
     });
 }
