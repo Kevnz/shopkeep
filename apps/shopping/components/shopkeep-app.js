eb1197d6f8ac747e1a2dacf44bb1ae1281808f8b
Kevin Isom <kevin.isom@gmail.com>
2015-04-15 07:55:51 -0400

    Start bring the bits together

--- apps/shopping/components/shopkeep-app.js
+++ apps/shopping/components/shopkeep-app.js
@@ -1,18 +1,37 @@
+'use strict';
 import React from 'react';
-import LoginLink from './login-link';
+import ProductStore from '../stores/product-store';
+import ProductList from './product-list';
+function _getStateFromStores () {
+    console.log('_getStateFromStores');
+    var returnState = {
+        products: ProductStore.getState()
+    };
+    console.log(returnState);
+    return returnState;
+}
 
 export default class Shopkeep extends React.Component {
     constructor(props) {
         super(props);
+        this.state = _getStateFromStores();
     }
-
+    componentDidMount () {
+        ProductStore.listen(this._onChange.bind(this));
+    }
+    componentWillUnmount () {
+        ProductStore.unlisten(this._onChange.bind(this));
+    }
     render() {
         return (
             <div className="app">
-                <div className="pure-u">
-                <LoginLink />
-                </div>
+                <TopMenu />
+                <ProductList products={this.state.products}></ProductList>
             </div>
         );
+    }
+    _onChange () {
+        console.log('_onChange');
+        this.setState(_getStateFromStores());
     }
 }
