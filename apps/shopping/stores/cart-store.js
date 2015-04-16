4f39475c57049978158afc7a62f6be1b9b51f09f
Kevin Isom <kevin.isom@gmail.com>
2015-04-16 14:46:40 -0400

    Cart store, cause flux it.

---
+++ apps/shopping/stores/cart-store.js
@@ -0,0 +1,19 @@
+'use strict';
+import alt from '../alt.js';
+import ActionCreators from '../actions/action-creators';
+
+class CartStore {
+    constructor() {
+        this.bindActions(ActionCreators);
+        this.count = 0;
+    }
+    onReceiveCartCount (cartCount) {
+        this.count = cartCount;
+    }
+    onAddToCart() {
+        console.log('onAddToCart');
+        this.count++;
+    }
+}
+
+module.exports = alt.createStore(CartStore);
