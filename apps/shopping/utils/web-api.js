c1e84143e9221da0b5dd47bab0a38a245419b9c4
Kevin Isom <kevin.isom@gmail.com>
2015-04-16 14:46:50 -0400

    Added cart calls

--- apps/shopping/utils/web-api.js
+++ apps/shopping/utils/web-api.js
@@ -3,7 +3,7 @@ import request from 'superagent';
 import ActionCreators from '../actions/action-creators';
 
 const PRODUCTS_URI = '/api/products';
-
+const CART_URI = '/api/cart';
 export default {
     getAllProducts: function () {
         request.get(PRODUCTS_URI)
@@ -15,6 +15,21 @@ export default {
     },
     getProduct: function (id) {
         request.get(PRODUCTS_URI + '/' + id)
+            .end((err, res) => { 
+                console.log(res);
+                ActionCreators.receiveProduct(res.body);
+            });
+    },
+    getCartCount: function () {
+       request.get(CART_URI)
+            .end((err, res) => { 
+                console.log(res);
+                ActionCreators.receiveCartCount(res.body.count);
+            }); 
+    },
+    addToCart: function (id, quantity = 1) {
+        request.post(CART_URI + '/add')
+            .send({ product: id, quantity: quantity })
             .end((err, res) => { 
                 console.log(res);
                 ActionCreators.receiveProduct(res.body);
