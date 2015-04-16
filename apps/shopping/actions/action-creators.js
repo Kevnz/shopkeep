8f45bd315cf4a54d29531d4bde7e44714b4851eb
Kevin Isom <kevin.isom@gmail.com>
2015-04-16 14:44:45 -0400

    Build out actions for adding to cart

--- apps/shopping/actions/action-creators.js
+++ apps/shopping/actions/action-creators.js
@@ -1,5 +1,22 @@
 'use strict';
 import alt from '../alt';
+import api from '../utils/web-api';
 
-module.exports = alt.generateActions('receiveProducts',
-    'productSelected');
+class ActionsCreators {
+    constructor() {
+        this.generateActions(
+                'receiveProducts',
+                'receiveCartCount',
+                'receiveProduct',
+                'productSelected',
+                'removeFromCart',
+                'checkoutStarted'
+            );
+    }
+    addToCart(productKey) {
+        this.dispatch(productKey);
+        api.addToCart(productKey);
+    }
+
+}
+alt.createActions(ActionsCreators, exports);
