5db3272c68319e97cbb373b22c0fc852d1899260
Kevin Isom <kevin.isom@gmail.com>
2015-04-18 09:39:37 -0400

    product route

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,10 @@ var router = express.Router();
 router.get('/', (req, res) => {
   res.render('index', { title: 'Shopkeep' });
 });
+/* GET home page. */
+router.get('/product/:slug', (req, res) => {
+    //will isomorhpic this bad boy at some point, just not today.
+  res.render('index', { title: 'Shopkeep' });
+});
 
 export default router;
