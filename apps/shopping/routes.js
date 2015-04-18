66ab5cfa66cbacfb2a16a24cdde5b92f5c4f1b04
Kevin Isom <kevin.isom@gmail.com>
2015-04-18 09:38:03 -0400

    Add checkout route

--- apps/shopping/routes.js
+++ apps/shopping/routes.js
@@ -4,9 +4,11 @@ import { Route, DefaultRoute  } from 'react-router';
 import Shopkeep from './components/shopkeep-app';
 import Products from './components/product-list';
 import ProductDetail from './components/product-detail';
+import Checkout from './components/checkout';
 export default (
     <Route name='main' path='/' handler={Shopkeep}>
         <DefaultRoute name="productlist" handler={Products} />
         <Route name='productlisting' path='/product/:slug' handler={ProductDetail} />
+        <Route name='checkout' path='/checkout' handler={Checkout} />
     </Route>
 );
