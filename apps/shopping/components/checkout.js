e69f3bea14e2bf7d6c19e3c1341f761d14a0f08e
Kevin Isom <kevin.isom@gmail.com>
2015-04-18 09:37:23 -0400

    Skeleton of checkout component

---
+++ apps/shopping/components/checkout.js
@@ -0,0 +1,18 @@
+'use strict';
+import React from 'react';
+
+export default class Checkout extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div> 
+                <div className="checkout"><h3>Checkout</h3></div>
+                <div className="">
+
+                </div>
+            </div>
+            );
+    }
+}
