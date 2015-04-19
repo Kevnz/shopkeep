044c95113bed61ed58f8e054e1c8a291cd172b3a
Kevin Isom <kevin.isom@gmail.com>
2015-04-19 17:30:25 -0400

    Checkout component

--- apps/shopping/components/checkout.js
+++ apps/shopping/components/checkout.js
@@ -1,16 +1,29 @@
 'use strict';
 import React from 'react';
+import ActionCreators from '../actions/action-creators';
+import { Router, Route, RouteHandler, Link } from 'react-router';
 
 export default class Checkout extends React.Component {
     constructor(props) {
         super(props);
     }
+    componentDidMount () {
+        console.log('cdm');
+        if(!this.props.cart.checkout){
+            console.log('cdm no co');
+             ActionCreators.checkoutStarted();
+        }
+    }
     render() {
+
+        let lineItems = !this.props.cart.checkout ? (<div>Nope</div>) : this.props.cart.checkout.lineItems.map((item) => {
+            return (<div> Item </div>);
+        })
         return (
             <div> 
                 <div className="checkout"><h3>Checkout</h3></div>
                 <div className="">
-
+                    {lineItems}
                 </div>
             </div>
             );
