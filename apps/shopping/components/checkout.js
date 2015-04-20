de8615cbc2de397b0d545a0a1a793252897f4982
Kevin Isom <kevin.isom@gmail.com>
2015-04-20 18:54:35 -0400

    tidy and flesh out

--- apps/shopping/components/checkout.js
+++ apps/shopping/components/checkout.js
@@ -1,30 +1,33 @@
 'use strict';
 import React from 'react';
 import ActionCreators from '../actions/action-creators';
-import { Router, Route, RouteHandler, Link } from 'react-router';
+import LineItem from './line-item';
+import accounting from 'accounting';
+import { Link } from 'react-router';
 
 export default class Checkout extends React.Component {
     constructor(props) {
         super(props);
     }
     componentDidMount () {
-        console.log('cdm');
         if(!this.props.cart.checkout){
-            console.log('cdm no co');
-             ActionCreators.checkoutStarted();
+            ActionCreators.checkoutStarted();
         }
     }
     render() {
 
-        let lineItems = !this.props.cart.checkout ? (<div>Nope</div>) : this.props.cart.checkout.lineItems.map((item) => {
-            return (<div> Item </div>);
-        })
+        let lineItems = !this.props.cart.checkout ? (<div>Nope</div>) : this.props.cart.checkout.contents.map((item) => {
+            return (<LineItem entry={item}></LineItem>);
+        });
+        let totalCost = !this.props.cart.checkout ? '0.00' : this.props.cart.checkout.totalCost;
         return (
-            <div> 
-                <div className="checkout"><h3>Checkout</h3></div>
-                <div className="">
+            <div className="checkout"> 
+                <h3>Checkout</h3>
+                <div className="checkout__line-items">
                     {lineItems}
                 </div>
+                <div className="checkout__total">Total: {accounting.formatMoney(totalCost)} </div>
+                <Link to="/details" className="checkout__btn">Checkout</Link>
             </div>
             );
     }
